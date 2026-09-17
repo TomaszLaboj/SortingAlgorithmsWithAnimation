cd329d536b752f6e87e38b02e8563541e06c4f26
tomaszlaboj <tomasz.laboj@gmail.com>
2026-09-17 18:53:39 +0100

    refactor

---
+++ src/main/java/sorting/sortingAlgorithms/SelectionSort.java
@@ -0,0 +1,24 @@
+package sorting.sortingAlgorithms;
+
+public class SelectionSort {
+
+   static void selectionSort(int[] nums) {
+       int minIndex;
+
+       for (int i = 0; i < nums.length; i++) {
+           minIndex = i;
+
+           for (int j = i; j < nums.length; j++) {
+               if (nums[j] < nums[minIndex]) {
+                   minIndex = j;
+               }
+           }
+           int temp = nums[i];
+           nums[i] = nums[minIndex];
+           nums[minIndex] = temp;
+       }
+   };
+
+
+
+}
